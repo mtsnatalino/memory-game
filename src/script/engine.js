524d14dfaa01eec69a1914384bec6421e5e72aef
mtsnatalino <matheuspedro982@gmail.com>
2024-10-15 12:02:33 -0300

    criando os cards dinamicamente

---
+++ src/script/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+    "😏",
+    "😏",
+    "😴",
+    "😴",
+    "🤑",
+    "🤑",
+    "🤪",
+    "🤪",
+    "😡",
+    "😡",
+    "🤡",
+    "🤡",
+    "💩",
+    "💩",
+    "😭",
+    "😭",
+]
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2: -1));
+
+for(let i = 0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
